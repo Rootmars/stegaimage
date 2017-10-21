2c80a689e79482340ac70ad3234a8b3f0a319e73
md <md@dank>
2017-10-20 20:59:14 -0700

    first

---
+++ README.md
@@ -0,0 +1,113 @@
+* Marvin Duro
+* 891583437
+* CPSC 353-01
+* Reza Nikoopour
+
+# StegaImage: Hiding secrets in plain sight.
+
+**StegaImage** is a program that allows a user to:
+
+1. write a hidden message to an image, and
+2. read a hidden message from an image.
+
+
+## Architecture
+
+A class called `StegaImage` encapsulates the functionalities required for
+`read`ing from, `write`ing to, and `save`ing a "steganography-enabled" image. 
+
+The command-line interface, powered by the `argparse` module, imitates 
+the interface of popular command line tools, such as `apt` or `openssl`.
+
+As an extension of the project, arbitrary binary data (read: other images!)
+can be written into an image.
+
+
+## Requirements
+
+This program requires `Python` and the `Pillow` module to run.
+
+
+## Instructions
+
+This program can be run in two modes: `read` and `write`
+
+### `read` Mode
+
+`read` mode reads a message from an image. The following command:
+
+	$ python stegaimage.py read input_image.png
+
+Will output the message hidden in `input_image.png` to stdout.
+
+To write output to a file instead, an optional third parameter can
+be provided. The following command:
+
+	$ python stegaimage.py read input_image.png output.txt
+
+Will write the message hidden in `input_image.png` to the 
+file `output.txt` instead.
+
+for more help regarding `read` mode, run:
+
+	$ python stegaimage.py read --help
+
+### `write` Mode
+
+`write` mode writes a message into an image. The following command:
+
+	$ python stegaimage.py write input_image.png output_image.png --phrase "Hello, World!"
+
+Will write the phrase "Hello, World!" into input_image.png, and saves the output
+to `ouput_image.png`
+
+The above example gets the input message from argv (via the `--phrase` argument.) The full
+list of ways in which to input the message is as follows:
+
+#### --stdin, -s
+
+The following command:
+
+	$ python stegaimage.py write input_image.png output_image.png --stdin < message_file.txt
+
+will take input from stdin (in this case, the file `message_file.txt` is redirected as input) 
+and hides the input into the image.
+
+If a file isn't redirected into stdin, then the user can input into the console directly. 
+Input can then be stopped with `EOF` (`Ctrl-D`).
+
+#### --file, -f
+
+The following command:
+
+	$ python stegaimage.py write input_image.png output_image.png --file message_file.txt
+
+will take input from the file called `message.txt` and hides the input into the image.
+
+#### --phrase, -p
+
+The following command:
+
+	$ python stegaimage.py write input_image.png output_image.png --phrase "Hello, World!"
+
+will hide the phrase "Hello, World!" into the image.
+
+for more help regarding `write` mode, run:
+
+	$ python stegaimage.py write --help
+
+### Extra Features
+
+Arbitrary data (not just textual data) can be written to an image.
+
+The following command:
+
+	$ python stegaimage.py write input_image.png output_image.png --file hidden_image.png
+
+Will hide `hidden_image.png` in `input_image.png` and saves the output to `output_image.png`
+
+
+To recover `hidden_image.png` from `output_image.png`, the following command can be run:
+
+	$ python stegaimage.py read output_image.png recovered_hidden_image.png
+
---
+++ stegaimage.py
@@ -0,0 +1,196 @@
+#!/usr/bin/python
+
+'''
+    Name: 
+        Marvin Duro
+    CWID: 
+        891583437
+    Filename: 
+        stegaimage.py
+    Description:
+        This program demonstrates steganography in the form of
+        hidden textual messages in images. View README.txt
+        for more details.
+'''
+
+from PIL import Image
+import sys
+import argparse
+
+class StegaImage:
+    def __init__(self, filename):
+        """Creates a new "steganography-enabled" image.
+        
+        Args:
+            filename: A path of the image to be opened.
+        """
+
+        self.image = Image.open(filename)
+        self.data = list(self.image.getdata())
+    
+    def _get_bit(self, offset):
+        """Gets the value of the bit at a specified offset.
+
+        Args:
+            offset: Distance from the bottom-left "bit" of the image.
+
+        Raises:
+            IndexError: If the specified offset is outside of the image's bounds.
+        """
+
+        offset_max = self._bit_limit()-1
+        if offset > offset_max: 
+            raise IndexError("Bit offset too large. Limit for this image is {}".format(offset_max))
+
+        pixel_index = -(offset + 1) // 3
+        pixel_data = self.data[pixel_index]
+        band_index = offset%3
+        return pixel_data[band_index] & 1
+
+    def _set_bit(self, offset, new_value):
+        """Sets the value of the bit at a specified offset.
+        
+        Args:
+            offset: Distance from the bottom-left "bit" of the image.
+            new_value: The new value for the bit. Must be 1 or 0.
+
+        Raises:
+            ValueError: If anything other than 1 or 0 is set as the new value.
+            IndexError: If the specified offset is outside of the image's bounds.
+        """
+
+        offset_max = self._bit_limit()-1
+        if offset > offset_max: 
+            raise IndexError("Bit offset too large. Limit for this image is {}".format(offset_max))
+
+        pixel_index = -(offset + 1) // 3
+        pixel_data = self.data[pixel_index]
+        pixel_data_as_list = list(pixel_data)
+        band_index = offset%3
+        if new_value == 1:
+            pixel_data_as_list[band_index] |= 1
+        elif new_value == 0:
+            pixel_data_as_list[band_index] &= ~1
+        else:
+            raise ValueError("A bit can only be set to 1 or 0.")
+
+        self.data[pixel_index] = tuple(pixel_data_as_list)
+
+    def _bit_limit(self):
+        """Gets the maximum number of encodable bits in the image."""
+
+        return len(self.data) * 3
+
+    def save(self, filename):
+        """Saves the image at its current state to a new file.
+
+        Note:
+            A file type of PNG (.png) is recommended.
+
+        Args:
+            filename: A path to while the image will be saved.
+                The file type is determined by the file extension.
+        """
+
+        self.image.putdata(self.data)
+        self.image.save(filename)
+
+    def read(self):
+        """Reads the message encoded in the image."""
+
+        # Read the length of the embedded message.
+        msg_length = 0
+        for i in range(0, 32):
+            bit = self._get_bit(i)
+            msg_length += bit << (31-i)
+
+        # Read the message character-by-character.
+        message = ""
+        for i in range(33, 33 + msg_length, 8):
+            char_bits = 0
+            for j in range(8):
+                char_bits += self._get_bit(i+j) << (7-j)
+
+            char = chr(char_bits)
+            message += char
+
+        return bytearray(message)
+
+    def write(self, phrase):
+        """Writes a new phrase onto the image.
+
+        Args:
+            phrase: The message that will be embedded onto the image.
+        """
+
+        # Length-encoding step.
+        length_in_bits = len(phrase)*8
+        for i in range(0, 32):
+            self._set_bit(i, (length_in_bits >> (31-i)) & 1)
+        
+        # Phrase-encoding step.
+        phrase_as_bytes = bytearray(phrase)
+        for i in range(0, length_in_bits):
+            byte_index = (i) // 8
+            bit_offset = 7 - (i%8)
+            self._set_bit(33 + i, (phrase_as_bytes[byte_index] >> (bit_offset)) & 1)
+
+        return
+
+# argparse setup starts here.
+# Callback for the "write" mode.
+def write_command(args):
+    stega_img = StegaImage(args.input_image)
+    if args.phrase:
+        message = bytearray(args.phrase)
+    elif args.stdin:
+        message = bytearray(sys.stdin.read())
+    elif args.file:
+        with open(args.file, "rb") as input_file:
+            message = bytearray(input_file.read())
+
+    stega_img.write(message)
+    stega_img.save(args.output_image)
+
+# Callback for the "read" mode.
+def read_command(args):
+    stega_img = StegaImage(args.input_image)
+    message = stega_img.read()
+    if args.output_file:
+        with open(args.output_file, "wb") as f:
+            f.write(message)
+    else:
+        print(message)
+
+parser = argparse.ArgumentParser()
+
+# This program has two modes: read and write.
+subparsers = parser.add_subparsers()
+
+# Read mode usage:
+# $ stegaimage read INPUT_IMAGE
+# -- Will print out the message embedded in INPUT_IMAGE
+read_parser = subparsers.add_parser("read", 
+    help="""reads the embedded message in an image and outputs the message to stdout.
+    enter `./stegaimage.py read --help` for more info.""")
+read_parser.set_defaults(func=read_command)
+read_parser.add_argument("input_image", help="a path to an image from which a hidden message will be read.")
+read_parser.add_argument("output_file", nargs="?", type=str, help="a path to a file nto which the result will be saved")
+
+# Write mode usage:
+# $ stegaimage write INPUT_IMAGE OUTPUT_IMAGE [--phrase | --stdin | --file]
+# -- Will embed a message into INPUT_IMAGE and saves the results into OUTPUT_IMAGE
+# -- Input can be a phrase (from argv directly), from stdin, or from a file.
+write_parser = subparsers.add_parser("write", 
+    help="""embeds a message into an image.
+    enter `./stegaimage.py write --help` for more info.""")
+write_parser.add_argument("input_image", type=str, help="a path to an image into which the message will be embedded")
+write_parser.add_argument("output_image", type=str, help="a path to an image into which the result will be saved")
+write_parser.set_defaults(func=write_command)
+phrase_group = write_parser.add_mutually_exclusive_group()
+phrase_group.add_argument("-p", "--phrase", help="embed a brief phrase into the image")
+phrase_group.add_argument("-s", "--stdin", help="embed input from stdin into the image", action="store_true")
+phrase_group.add_argument("-f", "--file", help="embed input from a file into the image")
+
+args = parser.parse_args()
+args.func(args)
